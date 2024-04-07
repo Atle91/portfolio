4c9e4a4d024902785b6c469023a3c821882892e5
Atle Kristiansen <atlekri@hotmail.com>
2024-04-07 22:28:27 +0200

    Created ME section

--- src/lib/components/AboutMe.svelte
+++
@@ -1,4 +0,0 @@
-<section id="about-me">
-	<h2>About Me</h2>
-	<!-- Add your content here -->
-</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script>
- 	import Header from '$lib/components/header/Header.svelte';
-	import AboutMe from '$lib/components/AboutMe.svelte';
-	import Skills from '$lib/components/Skills.svelte';
-	import MyWork from '$lib/components/my-work/MyWork.svelte';
+  import Header from '$lib/components/header/Header.svelte';
+  import AboutMe from '$lib/components/AboutMe/AboutMe.svelte';
+  import Skills from '$lib/components/Skills.svelte';
+  import MyWork from '$lib/components/my-work/MyWork.svelte';
 
-  import "./home.scss";
-  import "../global.scss";
+  import './home.scss';
+  import '../global.scss';
 </script>
 
 <Header />
 
 <main class="main">
-	<AboutMe />
-	<Skills />
-	<MyWork />
+  <AboutMe />
+  <Skills />
+  <MyWork />
 </main>
